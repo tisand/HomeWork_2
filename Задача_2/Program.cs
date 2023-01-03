f9a939d954b4f2fa24e2b7f195e2e907e730cfb6
Bekbolsun Alkozhoev <alkozhoev.b@gmail.com>
2023-01-03 23:05:30 +0500

    Задача_2

---
+++ Задача_2/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Write("Введите ваше число: ");
+string midNum = Console.ReadLine()??"";
+
+if (midNum.Length >= 3)
+{
+    Console.WriteLine(midNum[2]);
+}
+else
+{
+    Console.WriteLine("Третьего числа нет");
+}
